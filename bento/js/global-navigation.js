6263dcc110075b71a5b57cde98f7807f673a4d65
Thomas Schmidt <tschmidt@suse.de>
2011-02-23 14:24:32 +0100

    fix positioning of menues with jquery 1.4.4

--- bento/js/global-navigation.js
+++ bento/js/global-navigation.js
@@ -1,7 +1,6 @@
 var position_menu = function(button_id, menu_id) {
-    var top = $('#global-navigation').height()-12;
-    if ($.browser.webkit) top += 1;
-    var left = $('#' + button_id).offset().left-15;
+    var top = $('#global-navigation').height()+1;
+    var left = $('#' + button_id).offset().left;
     $('#' + menu_id).css({left:'',top:''});
     $('#' + menu_id).offset({left:left,top:top});
 }
